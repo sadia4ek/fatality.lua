8e3dc4c3e68d09ddf69579cb82fb2305185963d6
sadia4ek <kapybara1122@gmail.com>
2025-11-16 23:27:56 +0200

    desync yo

--- mainscript.lua
+++ mainscript.lua
@@ -17,7 +17,7 @@ local Window = Rayfield:CreateWindow({
    DisableBuildWarnings = false, 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = fatality,lua, 
+      FolderName = "fatality_lua", 
       FileName = "fatalitycfg.lua"
    },
    Discord = {
@@ -37,6 +37,11 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
+-- Services
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
 -- tabs
 local plrtab = Window:CreateTab("Player", "rewind") 
 local aatab = Window:CreateTab("Anti-Aim", "rewind") 
@@ -45,6 +50,22 @@ local misctab = Window:CreateTab("Misc", "rewind")
 local visaulstab = Window:CreateTab("Visuals", "rewind") 
 local infotab = Window:CreateTab("Info", "rewind") 
 
+-- Desync Variables
+local desyncT = {enabled = false, loc = CFrame.new()}
+local prevLookVector = nil
+local isSpinning = false
+local spinThreshold = 15
+
+-- Desync Settings
+local desyncSettings = {
+    customEnabled = false,
+    offsetX = 0,
+    offsetY = 0,
+    offsetZ = -2,
+    autoPause = true
+}
+
+-- playertab
 local changewalkksped = plrtab:CreateSlider({
     Name = "Change Walkspeed",
     Range = {1, 125},
@@ -82,6 +103,163 @@ local changejumpheit = plrtab:CreateSlider({
         end
     end,
 })
+
+local DesyncSection = misctab:CreateSection("Desync")
+
+local DesyncToggle = misctab:CreateToggle({
+    Name = "Desync Enabled",
+    CurrentValue = false,
+    Flag = "DesyncToggle",
+    Callback = function(Value)
+        desyncT.enabled = Value
+        if Value then
+            Rayfield:Notify({
+                Title = "Desync Activated",
+                Content = "Client-side desync is now active",
+                Duration = 3,
+            })
+        end
+    end,
+})
+
+local DesyncKeybind = misctab:CreateKeybind({
+    Name = "Desync Toggle Keybind",
+    CurrentKeybind = "C",
+    HoldToInteract = false,
+    Flag = "DesyncKeybind",
+    Callback = function(Keybind)
+        desyncT.enabled = not desyncT.enabled
+        DesyncToggle:Set(desyncT.enabled)
+    end,
+})
+
+local CustomDesyncToggle = misctab:CreateToggle({
+    Name = "Custom Desync Offset",
+    CurrentValue = false,
+    Flag = "CustomDesyncToggle",
+    Callback = function(Value)
+        desyncSettings.customEnabled = Value
+    end,
+})
+
+local OffsetXSlider = misctab:CreateSlider({
+    Name = "Offset X",
+    Range = {-10, 10},
+    Increment = 0.1,
+    Suffix = "studs",
+    CurrentValue = 0,
+    Flag = "OffsetX",
+    Callback = function(Value)
+        desyncSettings.offsetX = Value
+    end,
+})
+
+local OffsetYSlider = misctab:CreateSlider({
+    Name = "Offset Y",
+    Range = {-10, 10},
+    Increment = 0.1,
+    Suffix = "studs",
+    CurrentValue = 0,
+    Flag = "OffsetY",
+    Callback = function(Value)
+        desyncSettings.offsetY = Value
+    end,
+})
+
+local OffsetZSlider = misctab:CreateSlider({
+    Name = "Offset Z",
+    Range = {-10, 10},
+    Increment = 0.1,
+    Suffix = "studs",
+    CurrentValue = -2,
+    Flag = "OffsetZ",
+    Callback = function(Value)
+        desyncSettings.offsetZ = Value
+    end,
+})
+
+local AutoPauseToggle = misctab:CreateToggle({
+    Name = "Auto Pause When Spinning",
+    CurrentValue = true,
+    Flag = "AutoPauseToggle",
+    Callback = function(Value)
+        desyncSettings.autoPause = Value
+    end,
+})
+
+local SpinThresholdSlider = misctab:CreateSlider({
+    Name = "Spin Detection Threshold",
+    Range = {5, 45},
+    Increment = 1,
+    Suffix = "degrees",
+    CurrentValue = 15,
+    Flag = "SpinThreshold",
+    Callback = function(Value)
+        spinThreshold = Value
+    end,
+})
+
+local function getOffsetCFrame()
+    if desyncSettings.customEnabled then
+        return CFrame.new(desyncSettings.offsetX, desyncSettings.offsetY, desyncSettings.offsetZ)
+    else
+        local ping = LocalPlayer:GetNetworkPing() * 1000
+        if ping < 100 then return CFrame.new(0, 0, -2)
+        elseif ping <= 170 then return CFrame.new(0, 0, -2.7)
+        else return CFrame.new(0, 0, -3.7) end
+    end
+end
+
+RunService.Heartbeat:Connect(function()
+    if not desyncT.enabled or not LocalPlayer.Character then return end
+    
+    local character = LocalPlayer.Character
+    local root = character:FindFirstChild("HumanoidRootPart")
+    if not root then return end
+    
+    if desyncSettings.autoPause then
+        local currentLook = root.CFrame.LookVector
+        if prevLookVector then
+            local dot = math.clamp(prevLookVector:Dot(currentLook), -1, 1)
+            local angleDiff = math.deg(math.acos(dot))
+            isSpinning = angleDiff > spinThreshold
+        end
+        prevLookVector = currentLook
+        
+        if isSpinning then return end
+    end
+    
+    desyncT.loc = root.CFrame
+    
+    local offset = getOffsetCFrame()
+    local newCFrame = desyncT.loc * offset
+    root.CFrame = newCFrame
+    
+    RunService.RenderStepped:Wait()
+    root.CFrame = desyncT.loc
+end)
+
+local desynchook = nil
+desynchook = hookmetamethod(game, "__index", newcclosure(function(self, key)
+    if desyncT.enabled and not checkcaller() and 
+       key == "CFrame" and 
+       LocalPlayer.Character and 
+       self == LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and
+       not (desyncSettings.autoPause and isSpinning) then
+        return desyncT.loc
+    end
+    return desynchook(self, key)
+end))
+
+LocalPlayer.CharacterAdded:Connect(function()
+    task.wait(1)
+    prevLookVector = nil
+    isSpinning = false
+end)
+l
+
+
+
 -- zxc
 
 
